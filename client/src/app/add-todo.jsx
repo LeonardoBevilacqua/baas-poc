96054499d679bfccbbc29fbf30b496b4336ab511
Leonardo Almeida Bevilacqua <lbevilacqua@lenovo.com>
2023-10-05 13:17:46 -0300

    adding form

--- client/src/app/add-todo.jsx
+++ client/src/app/add-todo.jsx
@@ -1,3 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
 export default function AddTodo() {
-  return <div>AddTodo</div>;
+  const [description, setDescription] = useState("");
+
+  function submit(e) {
+    e.preventDefault();
+    alert(`submit ${description}`);
+    setDescription("");
+  }
+
+  return (
+    <div>
+      <form action="/action_page.php">
+        <fieldset>
+          <legend>Add new todo:</legend>
+          <label htmlFor="description">Description:</label>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <button onClick={submit}>Add</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
