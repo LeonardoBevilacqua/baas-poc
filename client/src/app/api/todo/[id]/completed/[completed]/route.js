0fc668eac67c75282821719f92165ac578384898
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2023-11-08 12:55:33 -0300

    removing user validation

--- client/src/app/api/todo/[id]/completed/[completed]/route.js
+++ client/src/app/api/todo/[id]/completed/[completed]/route.js
@@ -1,7 +1,4 @@
-import { unauthorized } from "@/app/api/http-response";
-import { AdminIdentityService } from "@/app/api/identity/identity.service";
 import { TodoService } from "@/app/api/todo/todo.service";
-import { AdminSupabaseIdentity } from "backend/infra/identity/supabase/admin-supabase.identity";
 import { TodoSupabaseRepository } from "backend/infra/repository/supabase/todo-supabase.repo";
 import { NextResponse } from "next/server";
 
@@ -14,18 +11,9 @@ export async function PATCH(request, { params }) {
   const todoService = new TodoService(
     TodoSupabaseRepository.Instance(request.cookies)
   );
-  const adminIdentityService = new AdminIdentityService(
-    AdminSupabaseIdentity.Instance(request.cookies)
-  );
-  const token = request.cookies.get("token").value;
-  if (!(await adminIdentityService.isUserLogged(token))) {
-    return unauthorized();
-  }
-
-  const userId = await adminIdentityService.getLoggedUserUid(token);
 
   const { id, completed } = params;
-  await todoService.update({ id, completed: completed === "true" }, userId);
+  await todoService.update({ id, completed: completed === "true" });
 
   return NextResponse.json(null, { status: 200 });
 }
--- client/src/app/api/todo/[id]/route.js
+++ client/src/app/api/todo/[id]/route.js
@@ -1,7 +1,4 @@
-import { unauthorized } from "@/app/api/http-response";
-import { AdminIdentityService } from "@/app/api/identity/identity.service";
 import { TodoService } from "@/app/api/todo/todo.service";
-import { AdminSupabaseIdentity } from "backend/infra/identity/supabase/admin-supabase.identity";
 import { TodoSupabaseRepository } from "backend/infra/repository/supabase/todo-supabase.repo";
 import { NextResponse } from "next/server";
 
@@ -14,17 +11,8 @@ export async function DELETE(request, { params }) {
   const todoService = new TodoService(
     TodoSupabaseRepository.Instance(request.cookies)
   );
-  const adminIdentityService = new AdminIdentityService(
-    AdminSupabaseIdentity.Instance(request.cookies)
-  );
-  const token = request.cookies.get("token").value;
-  if (!(await adminIdentityService.isUserLogged(token))) {
-    return unauthorized();
-  }
-
-  const userId = await adminIdentityService.getLoggedUserUid(token);
 
   const { id } = params;
-  await todoService.delete(id, userId);
+  await todoService.delete(id);
   return NextResponse.json(null, { status: 200 });
 }
